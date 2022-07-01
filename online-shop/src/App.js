3bcfde4cabc344da1571fcecb33e66d5c314200a
Med <mohammed.ahmadi1990@gmail.com>
2022-07-01 14:32:48 +0300

    category-container style
    category containers hierarchies are designed and connected in react

--- online-shop/src/App.js
+++ online-shop/src/App.js
@@ -2,18 +2,35 @@ import "./categories.styles.scss";
 
 const App = () => {
   const category = [
-    { id: 1, title: "Hats" },
-    { id: 2, title: "Jackets" },
-    { id: 3, title: "Sneakers" },
-    { id: 4, title: "Women" },
-    { id: 5, title: "Men" },
+    { id: 1, title: "Hats", imageUrl: "https://i.ibb.co/cvpntL1/hats.png" },
+    {
+      id: 2,
+      title: "Jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    },
+    {
+      id: 4,
+      title: "Women",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    },
+    { id: 5, title: "Men", imageUrl: "https://i.ibb.co/R70vBrQ/men.png" },
   ];
   return (
-    <div class="categories-container">
-      {category.map(({ title }) => (
-        <div class="category-container">
-          <dive class="background-image" />
-          <div class="category-body-container">
+    <div className="categories-container">
+      {category.map(({ title, id, imageUrl }) => (
+        <div key={id} className="category-container">
+          <div
+            className="background-image"
+            style={{
+              backgroundImage: `url(${imageUrl})`,
+            }}
+          />
+          <div className="category-body-container">
             <h2>{title}</h2>
             <p>Shop Now</p>
           </div>
