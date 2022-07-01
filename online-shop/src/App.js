0b4830ec8e4a79d37d8c1180451ff1cb9bc76f1b
Med <mohammed.ahmadi1990@gmail.com>
2022-07-01 13:42:02 +0300

    Sample data presentation

--- online-shop/src/App.js
+++ online-shop/src/App.js
@@ -1,5 +1,24 @@
 const App = () => {
-  return <div>Hello World!</div>;
+  const category = [
+    { id: 1, title: "Hats" },
+    { id: 2, title: "Jackets" },
+    { id: 3, title: "Sneakers" },
+    { id: 4, title: "Women" },
+    { id: 5, title: "Men" },
+  ];
+  return (
+    <div class="categories-container">
+      {category.map(({ title }) => (
+        <div class="category-container">
+          <dive class="background-image" />
+          <div class="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default App;
